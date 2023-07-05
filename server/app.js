dd98b827c3e55f41526f3dae4786a6c24a3a8967
Ivan Pietkiewicz <ivanciito_999@hotmail.com>
2023-07-05 14:34:49 -0300

    Se agragaron todo las importaciones y middlawares como la petcionon si la ruta no coinciden

--- server/app.js
+++ server/app.js
@@ -1,18 +1,24 @@
 // Imports
 const express = require("express");
 const cors = require("cors");
-const helmet = require("helmet");
 const morgan = require("morgan");
 const path = require("path");
 require("dotenv").config();
 require("ejs");
-
+const port = process.env.PORT || 5000;
 const app = express();
+
+const { sequelize } = require("./database");
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexión a base de datos exitosa");
+  })
+  .catch((error) => console.log("Error al conectar a base de datos", error));
 
 // Middlewares
 // TODO: Implementar middlewares
 app.use(cors());
-app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
