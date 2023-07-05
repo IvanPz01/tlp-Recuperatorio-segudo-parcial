1498a0698c857047561fdb5f9768f80bbcbe6dcd
Ivan Pietkiewicz <ivanciito_999@hotmail.com>
2023-07-05 14:39:41 -0300

    Se creo el modelo le la reserva

--- server/models/Reserva.js
+++ server/models/Reserva.js
@@ -1,1 +1,45 @@
 // TODO: Crear modelo de datos de Reserva
+
+const { sequelize, DataTypes } = require("../database");
+
+const cliente = sequelize.define(
+  "Cliente",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    codigo: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    apellido: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    fecha_ingreso: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  },
+  {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+  }
+);
+cliente.sync({ force: false }).then(() => {
+  console.log("Tabla de Reservas creada");
+});
+module.exports = cliente;
